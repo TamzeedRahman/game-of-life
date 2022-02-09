7748f6a1efea6f66caff97e048a58c7d2699483c
Tamzeed Rahman <tamzeedrahman@gmail.com>
2022-02-08 21:50:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HELLO MY OLD FRIEND
         </a>
       </header>
     </div>
